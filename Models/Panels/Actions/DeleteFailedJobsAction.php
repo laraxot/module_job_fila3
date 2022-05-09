6e316678792898e25fb5b879b8c351be5ea1b1b1
marco76tv <marco.sottana@gmail.com>
2022-05-09 18:31:09 +0200

    first

---
+++ Models/Panels/Actions/DeleteFailedJobsAction.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Job\Models\Panels\Actions;
+
+use Modules\Xot\Services\ArtisanService;
+use Modules\Xot\Models\Panels\Actions\XotBasePanelAction;
+
+
+/**
+ * Class ShowFailedJobAction.
+ */
+class DeleteFailedJobsAction extends XotBasePanelAction 
+
+
+
+{
+    public bool $onContainer = true;
+
+    public string $icon = '<i class="fas fa-dumpster-fire"></i>';
+
+    /**
+     * ArtisanAction constructor.
+     */
+    public function __construct() {
+    }
+
+    /**
+     * @return mixed
+     */
+    public function handle() {
+        $cmd='queue:flush';
+        $out = ArtisanService::act($cmd);
+        return $out.'<h3>+Done</h3>';
+    }
+
+    //end handle
+}
