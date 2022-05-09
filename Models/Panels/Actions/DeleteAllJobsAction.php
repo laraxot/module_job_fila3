d85a1fbaba5ac84ab90fd3a811fbba2153479dd6
Malebestia <malebestia@yahoo.it>
2022-05-09 19:19:36 +0200

    first

---
+++ Models/Panels/Actions/DeleteAllJobsAction.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Job\Models\Panels\Actions;
+
+use Modules\Xot\Services\ArtisanService;
+use Modules\Xot\Models\Panels\Actions\XotBasePanelAction;
+
+
+/**
+ * Class ShowFailedJobAction.
+ */
+class DeleteAllJobsAction extends XotBasePanelAction {
+    public bool $onContainer = true;
+
+    public string $icon = '<i class="fas fa-dumpster-fire"></i>';
+
+    /**
+     * ArtisanAction constructor.
+     */
+    public function __construct() {
+    }
+
+    /**
+     * @return mixed
+     */
+    public function handle() {
+        $cmd='queue:clear';
+        $out = ArtisanService::act($cmd);
+        $res=$this->rows->delete();
+        
+        return $out.'<h3>+Done ['.$res.'] deleted '.class_basename(__CLASS__).'</h3>';
+    }
+
+    //end handle
+}
