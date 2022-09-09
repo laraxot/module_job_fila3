9b2f52d59fba333633af7f36109af48dd5301d37
marco xot <marco.sottana@gmail.com>
2022-09-09 13:06:21 +0000

    up

--- Models/Panels/Policies/JobBatchPanelPolicy.php
+++ Models/Panels/Policies/JobBatchPanelPolicy.php
@@ -7,13 +7,10 @@ namespace Modules\Job\Models\Panels\Policies;
 use Modules\Xot\Models\Panels\Policies\XotBasePanelPolicy;
 
 class JobBatchPanelPolicy extends XotBasePanelPolicy {
-<<<<<<< HEAD
-=======
     /**
      * Undocumented function.
      */
     public function pruneBatches(?UserContract $user, PanelContract $panel): bool {
         return true;
     }
->>>>>>> bb58c29a38fec6af76ff96155b466b272beb677a
 }
