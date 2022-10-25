7148fa14a0293c7e30bd8719638eff1c940ffe00
marco76tv <marco.sottana@gmail.com>
2022-10-25 17:25:26 +0200

    rebase

--- Models/Panels/FailedJobPanel.php
+++ Models/Panels/FailedJobPanel.php
@@ -1,5 +1,6 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 <?php
 
 declare(strict_types=1);
@@ -255,3 +256,129 @@ class FailedJobPanel extends XotBasePanel {
 >>>>>>> 26a6287 (rebase)
 =======
 >>>>>>> 7563b33 (rebase)
+=======
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Job\Models\Panels;
+
+use Illuminate\Http\Request;
+use Modules\Xot\Models\Panels\Actions\ArtisanContainerAction;
+use Modules\Xot\Models\Panels\XotBasePanel;
+
+// --- Services --
+
+class FailedJobPanel extends XotBasePanel {
+    /**
+     * The model the resource corresponds to.
+     */
+    public static string $model = 'Modules\Xot\Models\FailedJob';
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     */
+    public static string $title = 'title';
+
+    /**
+     * Get the fields displayed by the resource.
+        'value'=>'..',
+     */
+    public function fields(): array {
+        return [
+            (object) [
+                'type' => 'Id',
+                'name' => 'id',
+                'comment' => null,
+            ],
+
+            (object) [
+                'type' => 'String',
+                'name' => 'uuid',
+                'rules' => 'required',
+                'comment' => null,
+            ],
+
+            (object) [
+                'type' => 'Text',
+                'name' => 'connection',
+                'rules' => 'required',
+                'comment' => null,
+            ],
+
+            (object) [
+                'type' => 'Text',
+                'name' => 'queue',
+                'rules' => 'required',
+                'comment' => null,
+            ],
+
+            (object) [
+                'type' => 'Text',
+                'name' => 'payload',
+                'rules' => 'required',
+                'comment' => null,
+            ],
+
+            (object) [
+                'type' => 'Text',
+                'name' => 'exception',
+                'rules' => 'required',
+                'comment' => null,
+            ],
+
+            (object) [
+                'type' => 'Datetime',
+                'name' => 'failed_at',
+                'rules' => 'required',
+                'comment' => null,
+            ],
+        ];
+    }
+
+    /**
+     * Get the tabs available.
+     */
+    public function tabs(): array {
+        $tabs_name = [];
+
+        return $tabs_name;
+    }
+
+    /**
+     * Get the cards available for the request.
+     */
+    public function cards(Request $request): array {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @param \Illuminate\Http\Request $request
+     */
+    public function filters(Request $request = null): array {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     */
+    public function lenses(Request $request): array {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     */
+    public function actions(): array {
+        return [
+            // new ArtisanContainerAction('queue:flush'),
+            new Actions\DeleteFailedJobsAction(),
+            new Actions\ShowFailedJobAction(),
+            new Actions\RetryFailedJobAction(),
+            new Actions\RetryAllFailedJobAction(),
+        ];
+    }
+}
+>>>>>>> 1f3dc29 (rebase)
