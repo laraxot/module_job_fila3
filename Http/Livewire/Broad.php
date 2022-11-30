d5417f4ea940adddf146069164a14214250dc7ca
marco xot <marco.sottana@gmail.com>
2022-11-30 17:42:42 +0100

    up

---
+++ Http/Livewire/Broad.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Job\Http\Livewire;
+
+use App\Events\PublicEvent;
+use Livewire\Component;
+
+class Broad extends Component {
+    protected $listeners = ['echo:public,PublicEvent' => 'notifyEvent'];
+
+    public function render() {
+        return view('job::livewire.broad');
+    }
+
+    public function try() {
+        session()->flash('message', 'try ['.now().']');
+        // OrderShipped::dispatch();
+        event(new PublicEvent('test'));
+    }
+
+    public function notifyEvent() {
+        session()->flash('message', 'notifyEvent ['.now().']');
+        dd('fine');
+        // $this->showNewOrderNotification = true;
+    }
+}
