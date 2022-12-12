cacc075b2d8f2f0aa32b43fdb5fe54ab761887fe
marco xot <marco.sottana@gmail.com>
2022-12-12 15:07:06 +0100

    up

--- Models/Panels/Actions/FixQueueNameAction.php
+++ Models/Panels/Actions/FixQueueNameAction.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Modules\Job\Models\Panels\Actions;
 
 use Illuminate\Queue\Jobs\Job;
-use Illuminate\Support\Str;
 use Modules\Job\Models\Job as JobModel;
 use Modules\Xot\Models\Panels\Actions\XotBasePanelAction;
 
@@ -21,30 +20,6 @@ class FixQueueNameAction extends XotBasePanelAction {
      * ArtisanAction constructor.
      */
     public function __construct() {
-    }
-
-    public function handleTEST() {
-        // $command='O:32:"Spatie\QueueableAction\ActionJob":4:{s:14:"*actionClass";s:48:"Modules\Mediamonitor\Actions\CreatePressFromData";s:13:"*parameters";a:1:{i:0;a:2:{s:8:"file_xml";s:99:"/mediamonitor/data/xml/NazionaliRadio/Radio24/2022/07/27/NazionaliRadio-Radio24-20220727-093000.xml";s:8:"file_mp4";s:108:"/mediamonitor/data/media/NazionaliRadio/Radio24/2022/07/27/NazionaliRadio-Radio24-20220727-093000-100000.mp4";}}s:10:"*backoff";i:3;s:5:"queue";s:60:"Modules\Mediamonitor\Http\Controllers\ApiController/addPress";}';
-        // dddx(unserialize($command));
-        $job = JobModel::first();
-        $command = $job->payload['data']['command'];
-        $command = unserialize($command);
-        // Spatie\QueueableAction\ActionJob
-        $action = app(\Modules\Mediamonitor\Actions\CreatePressFromData::class);
-        /*
-        dddx([
-            'comand'=>$command,
-            'methods'=>get_class_methods($command),
-            'parameters'=>$command->parameters(),
-            'parameters'=>$command->parameters(),
-            'action'=>$action,
-        ]);
-        */
-        $params = $command->parameters()[0];
-
-        $action->execute($params);
-
-        echo '<h3>DONE?? </h3>';
     }
 
     /**
@@ -64,20 +39,6 @@ class FixQueueNameAction extends XotBasePanelAction {
         // ]);
         echo '<table border="1">';
         foreach ($jobs as $job) {
-            /*
-            $queue_up=$job->queue;
-            $queue_up=Str::replace('Modules\Mediamonitor\Http\Controllers','',$queue_up);
-            $queue_up=Str::replace(['\\','/','controller'],[' ',' ',' '],$queue_up);
-            $queue_up=Str::replace(['modulesmediamonitorhttp-s'],[''],$queue_up);
-            $queue_up=Str::slug($queue_up);
-            echo '<tr>
-                <td>'.$job->getKey().'</td>
-                <td>'.$job->queue.'</td>
-                <td>'.$queue_up.'</td>
-                <td><pre>'.print_r($job->payload,true).'</pre></td>
-            </tr>';
-            $job->update(['queue'=>$queue_up]);
-            */
             $job->update(['queue' => 'queue01']);
         }
         echo '</table>';
