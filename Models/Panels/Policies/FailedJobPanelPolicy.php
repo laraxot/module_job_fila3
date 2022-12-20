cfc9e8c61c955c1c0852188fe46e45a7ae2f8590
marco76tv <marco.sottana@gmail.com>
2022-12-20 14:52:42 +0100

    up

--- Models/Panels/Policies/FailedJobPanelPolicy.php
+++ Models/Panels/Policies/FailedJobPanelPolicy.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace Modules\Job\Models\Panels\Policies;
 
-use Modules\Xot\Contracts\PanelContract;
+use Modules\Cms\Contracts\PanelContract;
 use Modules\Xot\Contracts\UserContract;
-use Modules\Xot\Models\Panels\Policies\XotBasePanelPolicy;
+use Modules\Cms\Models\Panels\Policies\XotBasePanelPolicy;
 
 class FailedJobPanelPolicy extends XotBasePanelPolicy {
     public function artisanContainer(?UserContract $user, PanelContract $panel): bool {
