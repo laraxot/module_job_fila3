35ebae63a91dc24cca9e67d6b6dfb6856dfd6e2c
marco xot <marco.sottana@gmail.com>
2023-03-13 18:35:03 +0100

    up

---
+++ Models/Task.php
@@ -0,0 +1,242 @@
+<?php
+
+namespace Modules\Job\Models;
+
+use Carbon\Carbon;
+use Cron\CronExpression;
+use Database\Factories\TotemTaskFactory;
+use Exception;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
+use Studio\Totem\Traits\FrontendSortable;
+use Studio\Totem\Traits\HasFrequencies;
+
+class Task extends BaseModel
+{
+    use Notifiable, HasFrequencies, FrontendSortable, HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id',
+        'description',
+        'command',
+        'parameters',
+        'expression',
+        'timezone',
+        'is_active',
+        'dont_overlap',
+        'run_in_maintenance',
+        'notification_email_address',
+        'notification_phone_number',
+        'notification_slack_webhook',
+        'auto_cleanup_type',
+        'auto_cleanup_num',
+        'run_on_one_server',
+        'run_in_background',
+    ];
+
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
+    protected $appends = [
+        'activated',
+        'upcoming',
+        'last_result',
+        'average_runtime',
+    ];
+
+    /**
+     * Activated Accessor.
+     *
+     * @return bool
+     */
+    public function getActivatedAttribute(): bool
+    {
+        return $this->is_active ?? true;
+    }
+
+    /**
+     * Upcoming Accessor.
+     *
+     * @return string
+     *
+     * @throws Exception
+     */
+    public function getUpcomingAttribute(): string
+    {
+        return CronExpression::factory($this->getCronExpression())->getNextRunDate()->format('Y-m-d H:i:s');
+    }
+
+    /**
+     * Convert a string of command arguments and options to an array.
+     *
+     * @param  bool  $console  if true will convert arguments to non associative array
+     * @return array
+     */
+    public function compileParameters(bool $console = false): array
+    {
+        if ($this->parameters) {
+            $regex = '/(?=\S)[^\'"\s]*(?:\'[^\']*\'[^\'"\s]*|"[^"]*"[^\'"\s]*)*/';
+            preg_match_all($regex, $this->parameters, $matches, PREG_SET_ORDER, 0);
+
+            $argument_index = 0;
+
+            $duplicate_parameter_index = function (array $carry, array $param, string $trimmed_param) {
+                if (! isset($carry[$param[0]])) {
+                    $carry[$param[0]] = $trimmed_param;
+                } else {
+                    if (! is_array($carry[$param[0]])) {
+                        $carry[$param[0]] = [$carry[$param[0]]];
+                    }
+                    $carry[$param[0]][] = $trimmed_param;
+                }
+
+                return $carry;
+            };
+
+            return collect($matches)->reduce(function ($carry, $parameter) use ($console, &$argument_index, $duplicate_parameter_index) {
+                $param = explode('=', $parameter[0], 2);
+
+                if (count($param) > 1) {
+                    $trimmed_param = trim(trim($param[1], '"'), "'");
+                    if ($console) {
+                        if (Str::startsWith($param[0], ['--', '-'])) {
+                            $carry = $duplicate_parameter_index($carry, $param, $trimmed_param);
+                        } else {
+                            $carry[$argument_index++] = $trimmed_param;
+                        }
+
+                        return $carry;
+                    }
+
+                    return $duplicate_parameter_index($carry, $param, $trimmed_param);
+                }
+
+                Str::startsWith($param[0], ['--', '-']) && ! $console ?
+                    $carry[$param[0]] = true :
+                    $carry[$argument_index++] = $param[0];
+
+                return $carry;
+            }, []);
+        }
+
+        return [];
+    }
+
+    /**
+     * Results Relation.
+     *
+     * @return HasMany
+     */
+    public function results(): HasMany
+    {
+        return $this->hasMany(Result::class, 'task_id', 'id');
+    }
+
+    /**
+     * Returns the most recent result entry for this task.
+     *
+     * @return Result|null
+     */
+    public function getLastResultAttribute(): Result|null
+    {
+        return $this->results()->orderBy('id', 'desc')->first();
+    }
+
+    /**
+     * @return float
+     */
+    public function getAverageRuntimeAttribute(): float
+    {
+        return $this->results()->avg('duration') ?? 0.00;
+    }
+
+    /**
+     * Route notifications for the mail channel.
+     *
+     * @return string
+     */
+    public function routeNotificationForMail(): string
+    {
+        return $this->notification_email_address;
+    }
+
+    /**
+     * Route notifications for the Nexmo channel.
+     *
+     * @return string
+     */
+    public function routeNotificationForNexmo(): string
+    {
+        return $this->notification_phone_number;
+    }
+
+    /**
+     * Route notifications for the Slack channel.
+     *
+     * @return string
+     */
+    public function routeNotificationForSlack(): string
+    {
+        return $this->notification_slack_webhook;
+    }
+
+    /**
+     * Attempt to perform clean on task results.
+     */
+    public function autoCleanup()
+    {
+        if ($this->auto_cleanup_num > 0) {
+            if ($this->auto_cleanup_type === 'results') {
+                $oldest_id = $this->results()
+                    ->orderBy('ran_at', 'desc')
+                    ->limit($this->auto_cleanup_num)
+                    ->get()
+                    ->min('id');
+                do {
+                    $rowsToDelete = $this->results()
+                        ->where('id', '<', $oldest_id)
+                        ->limit(50)
+                        ->getQuery()
+                        ->select('id')
+                        ->pluck('id');
+
+                    Result::query()
+                        ->whereIn('id', $rowsToDelete)
+                        ->delete();
+                } while ($rowsToDelete->count() > 0);
+            } else {
+                do {
+                    $rowsToDelete = $this->results()
+                        ->where('ran_at', '<', Carbon::now()->subDays($this->auto_cleanup_num - 1))
+                        ->limit(50)
+                        ->getQuery()
+                        ->select('id')
+                        ->pluck('id');
+
+                    Result::query()
+                        ->whereIn('id', $rowsToDelete)
+                        ->delete();
+                } while ($rowsToDelete->count() > 0);
+            }
+        }
+    }
+
+    /**
+     * Create a new factory instance for the model.
+     *
+     * @return TotemTaskFactory
+     */
+    protected static function newFactory(): TotemTaskFactory
+    {
+        return TotemTaskFactory::new();
+    }
+}
---
+++ Models/Traits/HasFrequencies.php
@@ -0,0 +1,199 @@
+<?php
+
+namespace Modules\Job\Models\Traits;
+
+use Closure;
+use Illuminate\Console\Scheduling\ManagesFrequencies;
+use Illuminate\Contracts\Filesystem\FileNotFoundException;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Arr;
+use function json_decode;
+use function request;
+use Studio\Totem\Frequency;
+use Studio\Totem\Task;
+
+trait HasFrequencies
+{
+    use ManagesFrequencies;
+
+    /**
+     * The array of filter callbacks.
+     *
+     * @var array
+     */
+    protected array $filters = [];
+
+    /**
+     * The array of reject callbacks.
+     *
+     * @var array
+     */
+    protected array $rejects = [];
+
+    /**
+     * Boot HasFrequencies Trait.
+     */
+    public static function bootHasFrequencies()
+    {
+        static::saved(function ($model) {
+            $model->afterSave();
+        });
+
+        static::deleting(function ($model) {
+            $model->beforeDelete();
+        });
+    }
+
+    /**
+     * When task is updated or created, we grab the input. If the type is set to frequency in input we try to either
+     * update or create the frequencies included in input else delete the frequency. If the type is not frequency and
+     * the task in question has frequencies saved in databased, delete them all.
+     */
+    public function afterSave()
+    {
+        $input = $this->processData();
+
+        if (isset($input['type'])) {
+            if ($input['type'] == 'frequency') {
+                foreach ($this->frequencies as $frequency) {
+                    if (! in_array($frequency->interval, collect($input['frequencies'])->pluck('interval')->toArray())) {
+                        $frequency->delete();
+                    }
+                }
+
+                foreach ($input['frequencies'] as $_frequency) {
+                    $this->frequencies()->updateOrCreate(Arr::only($_frequency, ['task_id', 'label', 'interval']));
+                }
+            } else {
+                $this->frequencies->each(function ($frequency) {
+                    $frequency->delete();
+                });
+            }
+        }
+    }
+
+    /**
+     * Task Deleted.
+     */
+    public function beforeDelete()
+    {
+        $this->frequencies->each(function ($frequency) {
+            $frequency->delete();
+        });
+
+        $this->results()->delete();
+    }
+
+    /**
+     * Frequencies Relation.
+     *
+     * @return HasMany
+     */
+    public function frequencies(): HasMany
+    {
+        return $this->hasMany(Frequency::class, 'task_id', 'id')->with('parameters');
+    }
+
+    /**
+     * Generate a cron expression from frequencies.
+     *
+     * @return string
+     */
+    public function getCronExpression(): string
+    {
+        if (! $this->expression) {
+            $this->expression = '* * * * *';
+
+            foreach ($this->frequencies as $frequency) {
+                call_user_func_array([$this, $frequency->interval], $frequency->parameters->pluck('value')->toArray());
+            }
+
+            $expression = $this->expression;
+
+            $this->expression = null;
+
+            return $expression;
+        }
+
+        return $this->expression;
+    }
+
+    /**
+     * Determine if the filters pass for the event.
+     *
+     * @param  Application  $app
+     * @return bool
+     */
+    public function filtersPass(Application $app): bool
+    {
+        foreach ($this->filters as $callback) {
+            if (! $app->call($callback)) {
+                return false;
+            }
+        }
+
+        foreach ($this->rejects as $callback) {
+            if ($app->call($callback)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Register a callback to further filter the schedule.
+     *
+     * @param  Closure  $callback
+     * @return $this
+     */
+    public function when(Closure $callback): static
+    {
+        $this->filters[] = $callback;
+
+        return $this;
+    }
+
+    /**
+     * Schedule the event to run between start and end time.
+     *
+     * @param  string  $startTime
+     * @param  string  $endTime
+     * @return $this
+     */
+    public function between($startTime, $endTime): static
+    {
+        return $this->when($this->inTimeInterval($startTime, $endTime));
+    }
+
+    /**
+     * Process input data. If its an import action we must find out if the imported json has frequencies or not and
+     * prepare data accordingly.
+     *
+     * @throws FileNotFoundException
+     */
+    private function processData(): array
+    {
+        $data = request()->all();
+
+        if (! request()->hasFile('tasks')) {
+            return $data;
+        }
+
+        $task = collect(json_decode(request()->file('tasks')->get()))
+            ->filter(function ($task) {
+                return $task->id === $this->id;
+            })
+            ->first();
+
+        if ($task && ($task->frequencies ?? false)) {
+            $data['type'] = 'frequency';
+            $data['frequencies'] = collect($task->frequencies)->map(function ($frequency) {
+                return (array) $frequency;
+            })->toArray();
+        }
+
+        return $data;
+    }
+}
