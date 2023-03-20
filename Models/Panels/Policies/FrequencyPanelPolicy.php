b1ed19f5d15020d072deb727a3e0284a62ce901c
marco76tv <marco.sottana@gmail.com>
2023-03-20 11:32:41 +0100

    up

--- Models/Panels/Policies/FrequencyPanelPolicy.php
+++ Models/Panels/Policies/FrequencyPanelPolicy.php
@@ -3,9 +3,9 @@ namespace Modules\Job\Models\Panels\Policies;
 
 use Illuminate\Auth\Access\HandlesAuthorization;
 use Modules\LU\Models\User as User;
-use Modules\Job\Models\Panels\Policies\TaskPanelPolicy as Post; 
+use Modules\Job\Models\Panels\Policies\FrequencyPanelPolicy as Post; 
 
 use Modules\Cms\Models\Panels\Policies\XotBasePanelPolicy;
 
-class TaskPanelPolicy extends XotBasePanelPolicy {
+class FrequencyPanelPolicy extends XotBasePanelPolicy {
 }
