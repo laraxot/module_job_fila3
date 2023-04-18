7aa95ecaaa52135f80f4dd807740a537ef4b9c82
marco76tv <marco.sottana@gmail.com>
2023-04-18 13:09:22 +0200

    up

--- Models/Panels/ResultPanel.php
+++ Models/Panels/ResultPanel.php
@@ -1,27 +1,22 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Modules\Job\Models\Panels;
 
-use Illuminate\Http\Request;
-use Modules\Xot\Contracts\RowsContract;
 use Illuminate\Contracts\Support\Renderable;
-
-
+use Illuminate\Http\Request;
 use Modules\Cms\Models\Panels\XotBasePanel;
+use Modules\Xot\Contracts\RowsContract;
 
-class ResultPanel extends XotBasePanel
-{
+class ResultPanel extends XotBasePanel {
     /**
      * The model the resource corresponds to.
-     *
-     * @var string
      */
     public static string $model = 'Modules\Job\Models\Result';
 
     /**
      * The single value that should be used to represent the resource when being displayed.
-     *
-     * @var string
      */
     public static string $title = 'title';
 
@@ -30,57 +25,51 @@ class ResultPanel extends XotBasePanel
      *
      * @var array
      */
-    public static $search = array();
+    public static $search = [];
 
     /**
      * The relationships that should be eager loaded on index queries.
-     *
      */
-    public function with(): array
-    {
+    public function with(): array {
         return [];
     }
 
-    public function search(): array
-    {
-
+    public function search(): array {
         return [];
     }
 
     /**
-     * on select the option id
+     * on select the option id.
      *
      * quando aggiungi un campo select, è il numero della chiave
      * che viene messo come valore su value="id"
      *
-     * @param Modules\Job\Models\Result $row
+     * @param \Modules\Job\Models\Result $row
      *
      * @return int|string|null
      */
-    public function optionId($row)
-    {
+    public function optionId($row) {
         $key = $row->getKey();
-        if (null === $key || (!is_string($key) && !is_int($key))) {
-            throw new \Exception('[' . __LINE__ . '][' . class_basename(__CLASS__) . ']');
+        if (null === $key || (! is_string($key) && ! is_int($key))) {
+            throw new \Exception('['.__LINE__.']['.class_basename(__CLASS__).']');
         }
+
         return $key;
     }
 
     /**
      * on select the option label.
      *
-     * @param Modules\Job\Models\Result $row
+     * @param \Modules\Job\Models\Result $row
      */
-    public function optionLabel($row): string
-    {
+    public function optionLabel($row): string {
         return 'To Set';
     }
 
     /**
      * index navigation.
      */
-    public function indexNav(): ?Renderable
-    {
+    public function indexNav(): ?Renderable {
         return null;
     }
 
@@ -91,60 +80,46 @@ class ResultPanel extends XotBasePanel
      *
      * @return RowsContract
      */
-    public function indexQuery(array $data, $query)
-    {
-        //return $query->where('user_id', $request->user()->id);
+    public function indexQuery(array $data, $query) {
+        // return $query->where('user_id', $request->user()->id);
         return $query;
     }
 
-
-
     /**
      * Get the fields displayed by the resource.
-     *
-     * @return array
-        'col_size' => 6,
-        'sortable' => 1,
-        'rules' => 'required',
-        'rules_messages' => ['it'=>['required'=>'Nome Obbligatorio']],
         'value'=>'..',
      */
-    public function fields(): array
-    {
-        return array(
-
-            (object) array(
+    public function fields(): array {
+        return [
+            (object) [
                 'type' => 'Id',
                 'name' => 'id',
                 'comment' => 'not in Doctrine',
-            ),
-            (object) array(
+            ],
+            (object) [
                 'type' => 'Text',
                 'name' => 'ran_at',
                 'comment' => 'not in Doctrine',
-            ),
+            ],
 
-            (object) array(
+            (object) [
                 'type' => 'Text',
                 'name' => 'duration',
                 'comment' => 'not in Doctrine',
-            ),
+            ],
 
-            (object) array(
+            (object) [
                 'type' => 'Text',
                 'name' => 'result',
                 'comment' => 'not in Doctrine',
-            ),
-        );
+            ],
+        ];
     }
 
     /**
      * Get the tabs available.
-     *
-     * @return array
      */
-    public function tabs(): array
-    {
+    public function tabs(): array {
         $tabs_name = [];
 
         return $tabs_name;
@@ -152,11 +127,8 @@ class ResultPanel extends XotBasePanel
 
     /**
      * Get the cards available for the request.
-     *
-     * @return array
      */
-    public function cards(Request $request): array
-    {
+    public function cards(Request $request): array {
         return [];
     }
 
@@ -164,31 +136,22 @@ class ResultPanel extends XotBasePanel
      * Get the filters available for the resource.
      *
      * @param \Illuminate\Http\Request $request
-     *
-     * @return array
      */
-    public function filters(Request $request = null): array
-    {
+    public function filters(Request $request = null): array {
         return [];
     }
 
     /**
      * Get the lenses available for the resource.
-     *
-     * @return array
      */
-    public function lenses(Request $request): array
-    {
+    public function lenses(Request $request): array {
         return [];
     }
 
     /**
      * Get the actions available for the resource.
-     *
-     * @return array
      */
-    public function actions(): array
-    {
+    public function actions(): array {
         return [];
     }
 }
--- Models/Panels/TaskPanel.php
+++ Models/Panels/TaskPanel.php
@@ -4,14 +4,13 @@ declare(strict_types=1);
 
 namespace Modules\Job\Models\Panels;
 
-use Illuminate\Support\Str;
-use Illuminate\Http\Request;
-use Modules\Xot\Contracts\RowsContract;
-use Modules\Cms\Models\Panels\XotBasePanel;
 use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Modules\Cms\Models\Panels\XotBasePanel;
+use Modules\Xot\Contracts\RowsContract;
 
-class TaskPanel extends XotBasePanel
-{
+class TaskPanel extends XotBasePanel {
     /**
      * The model the resource corresponds to.
      */
@@ -32,13 +31,11 @@ class TaskPanel extends XotBasePanel
     /**
      * The relationships that should be eager loaded on index queries.
      */
-    public function with(): array
-    {
+    public function with(): array {
         return [];
     }
 
-    public function search(): array
-    {
+    public function search(): array {
         return [];
     }
 
@@ -48,15 +45,14 @@ class TaskPanel extends XotBasePanel
      * quando aggiungi un campo select, è il numero della chiave
      * che viene messo come valore su value="id"
      *
-     * @param Modules\Job\Models\Task $row
+     * @param \Modules\Job\Models\Task $row
      *
      * @return int|string|null
      */
-    public function optionId($row)
-    {
+    public function optionId($row) {
         $key = $row->getKey();
-        if (null === $key || (!is_string($key) && !is_int($key))) {
-            throw new \Exception('[' . __LINE__ . '][' . class_basename(__CLASS__) . ']');
+        if (null === $key || (! is_string($key) && ! is_int($key))) {
+            throw new \Exception('['.__LINE__.']['.class_basename(__CLASS__).']');
         }
 
         return $key;
@@ -65,18 +61,16 @@ class TaskPanel extends XotBasePanel
     /**
      * on select the option label.
      *
-     * @param Modules\Job\Models\Task $row
+     * @param \Modules\Job\Models\Task $row
      */
-    public function optionLabel($row): string
-    {
+    public function optionLabel($row): string {
         return Str::slug($row->command, '-');
     }
 
     /**
      * index navigation.
      */
-    public function indexNav(): ?Renderable
-    {
+    public function indexNav(): ?Renderable {
         return null;
     }
 
@@ -87,75 +81,70 @@ class TaskPanel extends XotBasePanel
      *
      * @return RowsContract
      */
-    public function indexQuery(array $data, $query)
-    {
+    public function indexQuery(array $data, $query) {
         // return $query->where('user_id', $request->user()->id);
         return $query;
     }
 
-    public function notificationFields(): array
-    {
+    public function notificationFields(): array {
         return [
             (object) [
                 'type' => 'Text',
                 'name' => 'notification_email_address',
                 'comment' => 'not in Doctrine',
-                'col_size' => 4
+                'col_size' => 4,
             ],
             (object) [
                 'type' => 'Text',
                 'name' => 'notification_phone_number',
                 'comment' => 'not in Doctrine',
-                'col_size' => 4
+                'col_size' => 4,
             ],
             (object) [
                 'type' => 'Text',
                 'name' => 'notification_slack_webhook',
                 'comment' => 'not in Doctrine',
-                'col_size' => 4
+                'col_size' => 4,
             ],
         ];
     }
 
-    public function autoCleanupFields(): array
-    {
+    public function autoCleanupFields(): array {
         return [
             (object) [
                 'type' => 'Text',
                 'name' => 'auto_cleanup_type',
                 'comment' => 'not in Doctrine',
-                'col_size' => 3
+                'col_size' => 3,
             ],
             (object) [
                 'type' => 'Text',
                 'name' => 'auto_cleanup_num',
                 'comment' => 'not in Doctrine',
-                'col_size' => 3
+                'col_size' => 3,
             ],
-
         ];
     }
 
-    public function commandFields(): array
-    {
+    public function commandFields(): array {
         return [
             (object) [
                 'type' => 'Text',
                 'name' => 'command',
                 'comment' => 'not in Doctrine',
-                'col_size' => 3
+                'col_size' => 3,
             ],
             (object) [
                 'type' => 'Text',
                 'name' => 'parameters',
                 'comment' => 'not in Doctrine',
-                'col_size' => 4
+                'col_size' => 4,
             ],
             (object) [
                 'type' => 'Text',
                 'name' => 'expression',
                 'comment' => 'not in Doctrine',
-                'col_size' => 3
+                'col_size' => 3,
             ],
             (object) [
                 'type' => 'Text',
@@ -167,38 +156,36 @@ class TaskPanel extends XotBasePanel
                 'type' => 'Text',
                 'name' => 'is_active',
                 'comment' => 'not in Doctrine',
-                'col_size' => 2
+                'col_size' => 2,
             ],
-
         ];
     }
 
-    public function runFields(): array
-    {
+    public function runFields(): array {
         return [
             (object) [
                 'type' => 'Integer',
                 'name' => 'dont_overlap',
                 'comment' => 'not in Doctrine',
-                'col_size' => 2
+                'col_size' => 2,
             ],
             (object) [
                 'type' => 'Integer',
                 'name' => 'run_in_maintenance',
                 'comment' => 'not in Doctrine',
-                'col_size' => 2
+                'col_size' => 2,
             ],
             (object) [
                 'type' => 'Integer',
                 'name' => 'run_on_one_server',
                 'comment' => 'not in Doctrine',
-                'col_size' => 2
+                'col_size' => 2,
             ],
             (object) [
                 'type' => 'Integer',
                 'name' => 'run_in_background',
                 'comment' => 'not in Doctrine',
-                'col_size' => 2
+                'col_size' => 2,
             ],
         ];
     }
@@ -207,8 +194,7 @@ class TaskPanel extends XotBasePanel
      * Get the fields displayed by the resource.
         'value'=>'..',
      */
-    public function fields(): array
-    {
+    public function fields(): array {
         return [
             (object) [
                 'type' => 'Id',
@@ -221,39 +207,35 @@ class TaskPanel extends XotBasePanel
                 'comment' => 'not in Doctrine',
             ],
 
-
-            (object)[
+            (object) [
                 'type' => 'Cell',
                 'name' => 'Command',
                 'fields' => $this->commandFields(),
             ],
 
-            (object)[
+            (object) [
                 'type' => 'Cell',
                 'name' => 'Notification',
                 'fields' => $this->notificationFields(),
             ],
-            (object)[
+            (object) [
                 'type' => 'Cell',
                 'name' => 'Auto Cleanup',
                 'fields' => $this->autoCleanupFields(),
             ],
 
-            (object)[
+            (object) [
                 'type' => 'Cell',
                 'name' => 'run ',
                 'fields' => $this->runFields(),
             ],
-
-
         ];
     }
 
     /**
      * Get the tabs available.
      */
-    public function tabs(): array
-    {
+    public function tabs(): array {
         $tabs_name = ['results', 'frequencies'];
 
         return $tabs_name;
@@ -262,8 +244,7 @@ class TaskPanel extends XotBasePanel
     /**
      * Get the cards available for the request.
      */
-    public function cards(Request $request): array
-    {
+    public function cards(Request $request): array {
         return [];
     }
 
@@ -272,24 +253,21 @@ class TaskPanel extends XotBasePanel
      *
      * @param \Illuminate\Http\Request $request
      */
-    public function filters(Request $request = null): array
-    {
+    public function filters(Request $request = null): array {
         return [];
     }
 
     /**
      * Get the lenses available for the resource.
      */
-    public function lenses(Request $request): array
-    {
+    public function lenses(Request $request): array {
         return [];
     }
 
     /**
      * Get the actions available for the resource.
      */
-    public function actions(): array
-    {
+    public function actions(): array {
         return [
             new Actions\ExecuteTaskPanelAction(),
         ];
