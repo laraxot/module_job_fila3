a996de2bbd744e4aacbf35744816f0dd23e4d780
marco76tv <marco.sottana@gmail.com>
2023-04-18 13:09:22 +0200

    up

--- Models/Panels/ParameterPanel.php
+++ Models/Panels/ParameterPanel.php
@@ -1,26 +1,22 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Modules\Job\Models\Panels;
 
-use Illuminate\Http\Request;
-use Modules\Xot\Contracts\RowsContract;
 use Illuminate\Contracts\Support\Renderable;
-
-
+use Illuminate\Http\Request;
 use Modules\Cms\Models\Panels\XotBasePanel;
+use Modules\Xot\Contracts\RowsContract;
 
 class ParameterPanel extends XotBasePanel {
     /**
      * The model the resource corresponds to.
-     *
-     * @var string
      */
     public static string $model = 'Modules\Job\Models\Parameter';
 
     /**
      * The single value that should be used to represent the resource when being displayed.
-     *
-     * @var string
      */
     public static string $title = 'title';
 
@@ -29,46 +25,45 @@ class ParameterPanel extends XotBasePanel {
      *
      * @var array
      */
-    public static $search = array (
-);
+    public static $search = [
+    ];
 
     /**
      * The relationships that should be eager loaded on index queries.
-     *
      */
-    public function with():array {
+    public function with(): array {
         return [];
     }
 
-    public function search() :array {
-
+    public function search(): array {
         return [];
     }
 
     /**
-     * on select the option id
+     * on select the option id.
      *
      * quando aggiungi un campo select, è il numero della chiave
      * che viene messo come valore su value="id"
      *
-     * @param Modules\Job\Models\Parameter $row
+     * @param \Modules\Job\Models\Parameter $row
      *
      * @return int|string|null
      */
     public function optionId($row) {
         $key = $row->getKey();
-        if(null===$key||(!is_string($key)&&!is_int($key))){
+        if (null === $key || (! is_string($key) && ! is_int($key))) {
             throw new \Exception('['.__LINE__.']['.class_basename(__CLASS__).']');
         }
+
         return $key;
     }
 
     /**
      * on select the option label.
      *
-     * @param Modules\Job\Models\Parameter $row
+     * @param \Modules\Job\Models\Parameter $row
      */
-    public function optionLabel($row):string {
+    public function optionLabel($row): string {
         return 'To Set';
     }
 
@@ -86,53 +81,39 @@ class ParameterPanel extends XotBasePanel {
      *
      * @return RowsContract
      */
-    public function indexQuery(array $data, $query)
-    {
-        //return $query->where('user_id', $request->user()->id);
+    public function indexQuery(array $data, $query) {
+        // return $query->where('user_id', $request->user()->id);
         return $query;
     }
 
-
-
     /**
      * Get the fields displayed by the resource.
-     *
-     * @return array
-        'col_size' => 6,
-        'sortable' => 1,
-        'rules' => 'required',
-        'rules_messages' => ['it'=>['required'=>'Nome Obbligatorio']],
         'value'=>'..',
      */
     public function fields(): array {
-        return array (
-  0 => 
-  (object) array(
-     'type' => 'Text',
-     'name' => 'id',
-     'comment' => 'not in Doctrine',
-  ),
-  1 => 
-  (object) array(
-     'type' => 'Text',
-     'name' => 'name',
-     'comment' => 'not in Doctrine',
-  ),
-  2 => 
-  (object) array(
-     'type' => 'Text',
-     'name' => 'value',
-     'comment' => 'not in Doctrine',
-  ),
-);
+        return [
+            0 => (object) [
+                'type' => 'Text',
+                'name' => 'id',
+                'comment' => 'not in Doctrine',
+            ],
+            1 => (object) [
+                'type' => 'Text',
+                'name' => 'name',
+                'comment' => 'not in Doctrine',
+            ],
+            2 => (object) [
+                'type' => 'Text',
+                'name' => 'value',
+                'comment' => 'not in Doctrine',
+            ],
+        ];
     }
 
     /**
      * Get the tabs available.
-     *
-     * @return array
      */
-    public function tabs():array {
+    public function tabs(): array {
         $tabs_name = [];
 
         return $tabs_name;
@@ -140,10 +121,8 @@ class ParameterPanel extends XotBasePanel {
 
     /**
      * Get the cards available for the request.
-     *
-     * @return array
      */
-    public function cards(Request $request):array {
+    public function cards(Request $request): array {
         return [];
     }
 
@@ -151,28 +130,22 @@ class ParameterPanel extends XotBasePanel {
      * Get the filters available for the resource.
      *
      * @param \Illuminate\Http\Request $request
-     *
-     * @return array
      */
-    public function filters(Request $request = null):array {
+    public function filters(Request $request = null): array {
         return [];
     }
 
     /**
      * Get the lenses available for the resource.
-     *
-     * @return array
      */
-    public function lenses(Request $request):array {
+    public function lenses(Request $request): array {
         return [];
     }
 
     /**
      * Get the actions available for the resource.
-     *
-     * @return array
      */
-    public function actions():array {
+    public function actions(): array {
         return [];
     }
 }
