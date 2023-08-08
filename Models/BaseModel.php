13ca640160b2dba9ef8ee43f2b8d83350f9bdf1f
marco76tv <marco.sottana@gmail.com>
2023-08-08 20:48:45 +0200

    up

--- Models/BaseModel.php
+++ Models/BaseModel.php
@@ -19,7 +19,7 @@ use Modules\Xot\Traits\Updater;
 abstract class BaseModel extends Model {
     use HasFactory;
     // use Searchable;
-    // use Cachable;
+    // //use Cachable;
     use Updater;
     /**
      * Indicates whether attributes are snake cased on arrays.
